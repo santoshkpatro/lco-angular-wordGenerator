c6b7311947ee2ab2163400165ae72f976e2e2ca5
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2020-06-10 22:06:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = ''
+  limit = 10;
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(" ")
+  }
 }
